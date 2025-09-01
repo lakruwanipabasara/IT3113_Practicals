45790537c9262761c7f6ec1b9eaf18d2bbe3956f
Lakruwani Pabasara <lakruwanipabasara14@gmail.com>
2025-09-01 14:30:46 +0530

    Create exercise.pl

---
+++ Day - 01/exercise.pl
@@ -0,0 +1,6 @@
+parent(tom,bob). /* Tom is a parent of Bob*/
+parent(pam,bob). /* Pam is a parent of Bob*/
+parent(tom,liz). /* Tom is a parent of Liz*/
+parent(bob,ann). /* Bob is a parent of Ann*/
+parent(pat,jim). /* Pat is a parent of Jim*/
+parent(bob,pat). /* Bob is a parent of Pat*/
